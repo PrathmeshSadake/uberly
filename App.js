a4d8cffe6c5b111b9aa7dec7f7831ae5e8c797e2
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-01-12 13:00:03 +0530

    created a global store using redux toolkit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <Provider store={}>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
--- slices/navSlice.js
+++ slices/navSlice.js
@@ -6,4 +6,28 @@ const initialState = {
   timeTravelInfo: null,
 };
 
-const navSlice = createSlice({});
+const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducer: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTimeTravelInfo: (state, action) => {
+      state.timeTravelInfo = action.payload;
+    },
+  },
+});
+
+export const { setOrigin, setDestination, setTimeTravelInfo } =
+  navSlice.actions;
+
+// Selectors
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTimeTravelInfo = (state) => state.nav.timeTravelInfo;
+
+export default navSlice.reducer;
